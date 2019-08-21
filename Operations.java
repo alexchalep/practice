ce4e07411f5c19eb8043c01ae738d171a5709674
alexchalep <47442079+alexchalep@users.noreply.github.com>
2019-08-21 21:30:40 +0300

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author alexchalep
+ */
+public class Operations {
+    public static int add(int x, int y)
+    {
+        int result = x + y;
+        return result;
+    }
+    public static int sub(int x, int y)
+    {
+        int result = x-y;
+        return result;
+    }
+    public static int mult(int x, int y)
+    {
+        int result = x*y;
+        return result;
+    }
+    public static int div(int x, int y)
+    {
+        int result = x/y;
+        return result;
+    }
+    
+}
